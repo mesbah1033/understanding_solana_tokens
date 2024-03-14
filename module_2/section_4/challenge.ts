3e9611bc2b87fcf504017402a1a310c558ffd451
mesbah u <misbah1033@gmail.com>
2024-03-14 06:21:58 +0000

    created challenge.ts for module_2 section_4

---
+++ module_2/section_4/challenge.ts
@@ -0,0 +1,16 @@
+// establishing CONNECTION
+
+
+// extracting payer (our) ADDRESS
+
+
+// create INSTRUCTION
+
+
+// create TRANSACTION
+
+
+// run TRANSACTION
+
+
+// confirm TRANSACTION and create TRANSACTION SIGNATURE
