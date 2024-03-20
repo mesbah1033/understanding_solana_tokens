e3e496d360b3e2359cfbeb001133a4efcbf19cf3
mesbah u <misbah1033@gmail.com>
2024-03-20 11:35:52 +0000

    Lab partially completed

--- module_2/section_5/solana-ping-frontend/components/WalletContextProvider.tsx
+++ module_2/section_5/solana-ping-frontend/components/WalletContextProvider.tsx
@@ -1,23 +1,25 @@
-import { FC, ReactNode } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { FC, ReactNode, useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import * as web3 from '@solana/web3.js'
-import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
-require('@solana/wallet-adapter-react-ui/styles.css');
+import * as web3 from "@solana/web3.js";
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const endpoint = web3.clusterApiUrl('devnet')
-	const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
-
-	return (
-		<ConnectionProvider endpoint={endpoint}>
-	    <WalletProvider wallets={wallets}>
-	      <WalletModalProvider>
-	        { children }
-        </WalletModalProvider>
+    const endpoint = web3.clusterApiUrl("devnet");
+    // not using wallets variable as we are going to be using the default value for wallets parameter 
+    const wallets = useMemo(() => [], []);
+  
+    return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]}>
+        <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
-	)
-}
+  );
+};
 
-export default WalletContextProvider
+export default WalletContextProvider;
